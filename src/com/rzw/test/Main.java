8955c44b7d5f0a8e7263949148fc73e179af07f0
ranzhiwei <15175657842@163.com>
2018-09-20 14:06:34 +0800

    123

--- src/com/rzw/test/Main.java
+++ src/com/rzw/test/Main.java
@@ -6,5 +6,10 @@ public class Main {
 	// write your code here
         System.out.println("Hello world!");
         System.out.println("你好！！");
+
+        int temp1 = 100;
+        System.out.println("temp1 = " + temp1);
+        int temp2 = 200;
+        System.out.println("temp2 = " + temp2);
     }
 }
